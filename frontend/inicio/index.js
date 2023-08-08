0fd7da1c4fdefdd47b1977f3f05116c700018e0d
josuegiraldos <jggiraldosuarez2411@gmail.com>
2023-08-08 00:33:50 -0500

    style: :art: Categorias
    Categorias at index.html - index.js

--- frontend/inicio/index.js
+++ frontend/inicio/index.js
@@ -3,9 +3,10 @@ import { getCategorias, postCategoria, deleteCategoria, putCategoria, getProduct
 document.addEventListener("DOMContentLoaded", () => {
     showCategorias();
     showCarrusel();
+    listarCategorias();
 });
 const contenedor = document.querySelector('#listaCategorias');
-const contenedorCarrusel = document.querySelector("#contenedorCarrusel");
+const listaCategorias = document.querySelector('#categoriasLista');
 
 async function showCategorias(){
     const categoriasResponse = await getCategorias();
@@ -20,79 +21,6 @@ async function showCategorias(){
     });
 }
 
-/* async function showCarrusel(){
-    const productosResponse = await getProductos();
-    console.log(productosResponse);
-    const productos = productosResponse.productos;
-
-    productos.forEach(producto => {
-        const { _id, nombre, descripcion, precio, imagen, tallas, colores, categoria, inventario} = producto;
-
-        const divGrande = document.createElement('div');
-        const divMedio = document.createElement('div');
-        const divPeque = document.createElement('div');
-        
-        divPeque.classList.add('p-3');
-        divPeque.setAttribute("style", "max-width: 700px;");
-        divPeque.innerHTML = 
-        `
-            <h4 class="text-light text-uppercase font-weight-medium mb-3">${descripcion}</h4>
-            <h3 class="display-4 text-white font-weight-semi-bold mb-4">${nombre}</h3>
-            <h4 class="display-4 text-white font-weight-semi-bold mb-4">$${precio}</h4>
-            <a href="#" class="btn btn-light py-2 px-3">Comprar</a>
-        `;
-
-        divMedio.classList.add('carousel-caption', 'd-flex', 'flex-column', 'align-items-center', 'justify-content-center');
-
-        divMedio.appendChild(divPeque);
-
-        divGrande.classList.add('carousel-item', 'active');
-        divGrande.setAttribute("style", "height: 410px;");
-        divGrande.innerHTML = 
-        `
-        <img class="img-fluid" src="../img/${imagen}" alt="Image">
-        `;
-        divGrande.appendChild(divMedio);
-
-        contenedorCarrusel.appendChild(divGrande);
-    });
-} */
-
-/* async function showCarrusel() {
-    const productosResponse = await getProductos();
-    console.log(productosResponse);
-    const productos = productosResponse.productos;
-  
-    const carruselContenedor = document.querySelector('.carousel-inner');
-  
-    carruselContenedor.innerHTML = ''; // Limpiar el contenido actual del carrusel
-  
-    productos.forEach((producto, index) => {
-      const { _id, nombre, descripcion, precio, imagen } = producto;
-  
-      const slide = document.createElement('div');
-      slide.classList.add('carousel-item');
-      if (index === 0) {
-        slide.classList.add('active');
-      }
-  
-      slide.innerHTML = `
-        <img class="d-block w-100 carousel-img" src="./img/${imagen}" alt="${nombre}" width="250px">
-        <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-          <h4>${descripcion}</h4>
-          <h3>${nombre}</h3>
-          <h4>Precio: $${precio}</h4>
-          <a href="#" class="btn btn-primary">Comprar</a>
-        </div>
-      `;
-  
-      carruselContenedor.appendChild(slide);
-    });
-  
-    // Inicializar el carrusel
-    $(carruselContenedor).parent().carousel();
-}  */
-
 async function showCarrusel() {
     const productosResponse = await getProductos();
     console.log(productosResponse);
@@ -100,7 +28,7 @@ async function showCarrusel() {
   
     const carruselContenedor = document.querySelector('.carousel-inner');
   
-    carruselContenedor.innerHTML = ''; // Limpiar el contenido actual del carrusel
+    carruselContenedor.innerHTML = '';
   
     productos.forEach((producto, index) => {
       const { _id, nombre, descripcion, precio, imagen } = producto;
@@ -126,7 +54,27 @@ async function showCarrusel() {
       carruselContenedor.appendChild(slide);
     });
   
-    // Inicializar el carrusel
     $(carruselContenedor).parent().carousel();
-  }
-  
+}
+ 
+async function listarCategorias(){
+    const categoriasResponse = await getCategorias();
+    const categorias = categoriasResponse.categorias;
+    categorias.forEach(categoria => {
+        const { _id, nombre, descripcion, imagen } = categoria;
+
+        const card = document.createElement('div');
+        card.innerHTML = 
+        `
+        <div class="card custom-card" style="width: 18rem;">
+            <img src="./img/${imagen}" class="card-img-top" alt="...">
+                <div class="card-body card-body d-flex flex-column justify-content-between">
+                    <h5 class="card-title">${nombre}</h5>
+                    <p class="card-text">${descripcion}</p>
+                    <a href="#" class="btn btn-primary">Ver más</a>
+                </div>
+        </div>
+        `;
+        listaCategorias.appendChild(card);
+    });
+}
